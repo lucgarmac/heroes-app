86083774cb673ee13ff6ed9aba38c32dcf432b82
Luciano <dlucianogarcia@gmail.com>
2024-03-10 13:38:38 +0100

    feat(footer): create footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToolbarModule } from './components/toolbar/toolbar.module';
+import { FooterModule } from './components/footer/footer.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ToolbarModule],
+  imports: [RouterOutlet, ToolbarModule, FooterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
